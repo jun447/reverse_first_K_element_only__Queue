a9aeef9d5e4c9c17768c4b585b0c08d17691e2a3
jun447 <fa20-bse-054@cuilahore.edu.pk>
2022-05-24 05:42:52 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,70 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+queue<int> modifyQueue(queue<int> q, int k);
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n, k;
+        cin >> n >> k;
+        queue<int> q;
+        while (n-- > 0) {
+            int a;
+            cin >> a;
+            q.push(a);
+        }
+        queue<int> ans = modifyQueue(q, k);
+        while (!ans.empty()) {
+            int a = ans.front();
+            ans.pop();
+            cout << a << " ";
+        }
+        cout << endl;
+    }
+}// } Driver Code Ends
+
+
+// User function Template for C++
+
+// Function to reverse first k elements of a queue.
+queue<int> modifyQueue(queue<int> q, int K) {
+    // pop frist K element 
+    // from Q and put in stack
+    stack<int> s;
+    for(int i=0;i<K;i++){
+        int val=q.front();
+        q.pop();
+        s.push(val);
+    }
+
+// step 2:  fetch from stack and put in Q
+    while(!s.empty()){
+        int val=s.top();
+        s.pop();
+        q.push(val);
+    }
+    // step 3 fetch first n-k element from Q and push back
+    int t=q.size()-K;
+    while(t--){
+        int val=q.front();
+        q.pop();
+        q.push(val);
+    }
+
+    return q;
+
+
+
+
+
+
+
+
+
+
+
+
+}
